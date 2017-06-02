0d4e3895268884d16d9ff0860166ad42271df13a
canepazzojoe <pietromeloni@hotmail.it>
2017-06-02 20:40:31 +0200

    ora la soglia funziona, triangoli rossi quando angoli piccoli

--- skeleton/skeletonmanager/drawableskel.cpp
+++ skeleton/skeletonmanager/drawableskel.cpp
@@ -76,15 +76,17 @@ void DrawableSkel::draw() const
         int vid0_ptr = 3 * vid0;
         int vid1_ptr = 3 * vid1;
         int vid2_ptr = 3 * vid2;
-        glBegin(GL_TRIANGLES);
         if(isTrisOnBorder(Pointd(coords[vid0_ptr],coords[vid0_ptr+1],coords[vid0_ptr+2]),
                           Pointd(coords[vid1_ptr],coords[vid1_ptr+1],coords[vid1_ptr+2]),
                           Pointd(coords[vid2_ptr],coords[vid2_ptr+1],coords[vid2_ptr+2])))
         {
+            glBegin(GL_TRIANGLES);
+
             glColor3f(1.0, 0.0, 0.0);
         }
         else
         {
+            glBegin(GL_TRIANGLES);
             glColor3f(0.1, 0.8, 0.1);
         }
         //glNormal3dv(&(triangleNormals[tid_ptr]));
@@ -107,12 +109,20 @@ void DrawableSkel::setEdgeList(std::list<std::pair<Pointd, Pointd>> edges)
 bool DrawableSkel::isTrisOnBorder(Pointd a, Pointd b, Pointd c)
 {
     double soglia = 3.14159265/18.0;
-    double abLenght = a.dist(b);
-    double acLenght = a.dist(c);
-    double bcLenght = b.dist(c);
-    double angle1 = acos((abLenght*abLenght+acLenght*acLenght - bcLenght*bcLenght)/2*abLenght*acLenght);
-    double angle2 = acos((bcLenght*bcLenght+acLenght*acLenght - abLenght*abLenght)/2*bcLenght*acLenght);
-    double angle3 = acos((abLenght*abLenght+bcLenght*bcLenght - acLenght*acLenght)/2*abLenght*bcLenght);
+
+    Pointd v1 (a.x() - b.x(), a.y() - b.y(), a.z() - b.z() );
+    Pointd v2 (b.x() - c.x(), b.y() - c.y(), b.z() - c.z() );
+    Pointd v3 (a.x() - c.x(), a.y() - c.y(), a.z() - c.z() );
+    v1.normalize();
+    v2.normalize();
+    v3.normalize();
+
+    double angle1 = acos(v1.dot(v2));
+    double angle2 = acos(v1.dot(v3));
+    double angle3 = acos(v2.dot(v3));
+
+
+    //qDebug() << "angolo1 " << angle1 << " angolo2 " << angle2 << " angolo3 " << angle3 << "\n";
 
     if(angle1 < soglia || angle2 <soglia || angle3 < soglia)
     {
